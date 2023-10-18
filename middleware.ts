85e08a8cbb4d18bc91158e1ee581e406cc89c12b
Rami Mhazres <superflava8@gmail.com>
2023-10-18 10:39:45 +0200

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
+export default {
   publicRoutes: ["/"],
-});
+};
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
